0e8b1611b150ac535489976d05318fcc4eb8ea1d
Franco <francoeinzerillo@gmail.com>
2022-11-07 20:52:55 -0300

    Application Initialized

---
+++ src/main/java/com/frank/negocioRopa/NegocioRopaApplication.java
@@ -0,0 +1,13 @@
+package com.frank.negocioRopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioRopaApplication.class, args);
+	}
+
+}
