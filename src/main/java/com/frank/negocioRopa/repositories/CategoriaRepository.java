89b74689a1ddd71633ed1c0220edfa66cb8daec4
Franco <francoeinzerillo@gmail.com>
2022-11-08 13:18:34 -0300

    Add categoria & marca repository

---
+++ src/main/java/com/frank/negocioRopa/repositories/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.frank.negocioRopa.repositories;
+
+import com.frank.negocioRopa.entities.Categoria;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends BaseRepository<Categoria, Long>{
+}
---
+++ src/main/java/com/frank/negocioRopa/repositories/MarcaRepository.java
@@ -0,0 +1,8 @@
+package com.frank.negocioRopa.repositories;
+
+import com.frank.negocioRopa.entities.Marca;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarcaRepository extends BaseRepository<Marca, Long>{
+}
